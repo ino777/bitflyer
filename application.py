866ff5477ef9174585fbc772a93ab9251d19ce20
Manato Inoue <tarou.xxx.0129@gmail.com>
2021-12-09 11:54:09 +0900

    Fix to deploy

--- application.py
+++ application.py
@@ -3,22 +3,20 @@ import threading
 
 from config import config
 from utils.logsettings import getLogger
-
+from app.controllers.webserver import app
 
 logger = getLogger(__name__)
 
 
-if __name__ == '__main__':
-    from bitflyer import bitflyer
-    from app.models import base, candle, events
-    base.init()
-    
-    from app.controllers import streamdata, webserver
-    
-    t = threading.Thread(target=streamdata.stream_ingestion_data)
-    t.setDaemon(True)
-    t.start()
+from bitflyer import bitflyer
+from app.models import base, candle, events
+base.init()
 
-    from app.controllers.webserver import app
-    app.debug = False
-    app.run()
+from app.controllers import streamdata, webserver
+
+t = threading.Thread(target=streamdata.stream_ingestion_data)
+t.setDaemon(True)
+t.start()
+
+app.debug = False
+app.run()
